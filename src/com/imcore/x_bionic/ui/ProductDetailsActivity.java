2e241d92b183ec6b3cfcf524462b10b1cebbf0c9
seven001 <731739299@qq.com>
2014-06-07 16:59:02 +0800

    .

--- src/com/imcore/x_bionic/ui/ProductDetailsActivity.java
+++ src/com/imcore/x_bionic/ui/ProductDetailsActivity.java
@@ -11,29 +11,21 @@ import com.imcore.x_bionic.http.RequestEntity;
 import com.imcore.x_bionic.http.ResponseJsonEntity;
 import com.imcore.x_bionic.image.ImageFetcher;
 import com.imcore.x_bionic.model.ProductDetails;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.widget.Toast;
 
-public class ProductDetailsActivity extends ActionBarActivity {
+public class ProductDetailsActivity extends Activity {
 	private List<ProductDetails> list;
 	private int status;
-	private int count;
-	private int id;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_details);
-		Intent intent =getIntent();
-		id = intent.getIntExtra("id", 0);
-		if (HttpHelper.isNetWokrConnected(this)) {
-			new getImageUrl().execute(id);
-		} else {
-			Toast.makeText(this, "网络不可用！", Toast.LENGTH_SHORT).show();
-		}
 		
 	}
 	private class getImageUrl extends AsyncTask<Integer , Void, Void>{
@@ -58,14 +50,6 @@ public class ProductDetailsActivity extends ActionBarActivity {
 			}
 			return null;
 		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			for(int i= 0;i <list.size(); i++){
-				new DownLoadImages().execute("http://www.bulo2bulo.com/"+list.get(i).image + "_S.jpg");
-			}
-			super.onPostExecute(result);
-		}
 	}
 	private class DownLoadImages extends AsyncTask<String, Void, String> {
 
@@ -79,12 +63,6 @@ public class ProductDetailsActivity extends ActionBarActivity {
 
 		@Override
 		protected void onPostExecute(String result) {
-			if(count == list.size()) {
-				DetailsFragment fragment = new DetailsFragment();
-				Bundle bundle = new Bundle();
-				bundle.putInt("id", id);
-				fragment.setArguments(bundle);
-			}
 			super.onPostExecute(result);
 		}
 	}
