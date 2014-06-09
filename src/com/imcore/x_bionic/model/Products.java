383b060d289a6cffc853d7bf640e1235613206e3
seven001 <731739299@qq.com>
2014-06-09 18:05:01 +0800

    1

---
+++ src/com/imcore/x_bionic/model/Products.java
@@ -0,0 +1,13 @@
+package com.imcore.x_bionic.model;
+
+public class Products {
+	public int id;
+	public String name;
+	public int categoryId;
+	public int price;
+	public String serialNo;
+	public String description;
+	public int washCareStandardId;
+	public String imageUrl;
+	public String sysColorList;
+}
--- src/com/imcore/x_bionic/ui/ProductDetailsActivity.java
+++ src/com/imcore/x_bionic/ui/ProductDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.imcore.x_bionic.ui;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,21 +12,36 @@ import com.imcore.x_bionic.http.RequestEntity;
 import com.imcore.x_bionic.http.ResponseJsonEntity;
 import com.imcore.x_bionic.image.ImageFetcher;
 import com.imcore.x_bionic.model.ProductDetails;
+import com.imcore.x_bionic.model.Products;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ProductDetailsActivity extends Activity {
 	private List<ProductDetails> list;
-	private int status;
+	private Gallery gallery;
+	private Products products;
+	private TextView tvname,tvprice;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_product_details);
+		
+		Intent intent = getIntent();
+		int id  = intent.getIntExtra("id", 0);
+		gallery = (Gallery) findViewById(R.id.gl_detail);
+		new getImageUrl().execute(id);
 		
 	}
 	private class getImageUrl extends AsyncTask<Integer , Void, Void>{
@@ -35,9 +51,11 @@ public class ProductDetailsActivity extends Activity {
 			String url = "product/images/list.do";
 			Map<String, Object> map = new HashMap<String, Object>();
 			map.put("id", params[0]);
+			// 将请求实体存到一个对象中
 			RequestEntity  request = new RequestEntity(url, HttpMethod.GET, map);
 			String Json = "";
 			try {
+				// 得到请求的网络返回的结果
 				Json = HttpHelper.execute(request);
 				ResponseJsonEntity JsonEntity = ResponseJsonEntity
 						.fromJSON(Json);
@@ -50,21 +68,52 @@ public class ProductDetailsActivity extends Activity {
 			}
 			return null;
 		}
-	}
-	private class DownLoadImages extends AsyncTask<String, Void, String> {
-
 		@Override
-		protected String doInBackground(String... params) {
-			if (status == 200) {
-				ImageFetcher.downLoadImage(params[0]);
-			}
-			return params[0];
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
+		protected void onPostExecute(Void result) {
+			gallery.setAdapter(new GLAdarpter());
+			gallery.setSpacing(100);
 			super.onPostExecute(result);
 		}
+	}
+//	private class 
+	private class GLAdarpter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			return list.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return list.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			ViewHolder viewHolder = null;
+			if (view == null) {
+				view = getLayoutInflater().inflate(R.layout.view_item_detail, null);
+				viewHolder = new ViewHolder();
+				viewHolder.imageView = (ImageView) view.findViewById(R.id.iv_img);
+				view.setTag(viewHolder);
+			}else{
+				viewHolder = (ViewHolder) view.getTag();
+			}
+			String imgPath = "http://www.bulo2bulo.com"
+					+ list.get(position).image + "_L.jpg";
+			new ImageFetcher().fetch(imgPath, viewHolder.imageView);
+			return view;
+		}
+		class ViewHolder{
+			public ImageView imageView;
+		}
+
 	}
 
 }
